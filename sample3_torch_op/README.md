5989557352c02647304803cf0bdbefbb612dcc11
tanfeiyang <flying_tan@163.com>
2020-10-20 14:54:17 +0000

    [tanfeiyang/master] add sapmle3

---
+++ sample3_torch_op/README.md
@@ -0,0 +1,1 @@
+Hello Python, CUDA,  and C++ 
---
+++ sample3_torch_op/version.py
@@ -0,0 +1,6 @@
+# GENERATED VERSION FILE
+# TIME: Tue Oct 20 02:19:45 2020
+
+__version__ = '2.2.1+81ebcfc'
+short_version = '2.2.1'
+version_info = (2, 2, 1)
