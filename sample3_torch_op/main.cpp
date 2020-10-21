76f7d53975ca671c9c940fb283055f6a2f522cc6
tanfeiyang <flying_tan@163.com>
2020-10-21 08:19:57 +0000

    [tanfeiyang/master] update sample3

--- sample3_torch_op/main.cpp
+++ sample3_torch_op/main.cpp
@@ -1,7 +1,13 @@
 #include <torch/torch.h>
-#include <iostream>
+#include <iostream> 
+
+at::Tensor nms_cuda(const at::Tensor& dets, const float threshold);
 
 int main() {
-  torch::Tensor tensor = torch::eye(3);
-  std::cout << tensor << std::endl;
+  torch::Tensor tensor = torch::randn({3, 5}).cuda();
+
+  std::cout <<"The tensor is: "<< tensor << std::endl;
+
+  torch::Tensor res = nms_cuda(tensor, 0.5);
+  std::cout<<"The result is: "<< res<<std::endl;
 }
